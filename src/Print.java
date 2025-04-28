bbac199ce00b201dec9048df77dd67f8ae68967b
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-04-28 17:36:58 +0300

    задача 4.2.6 первая проверка

--- src/Print.java
+++ src/Print.java
@@ -5,29 +5,29 @@ import java.util.Arrays;
 
 public class Print {
     public static void print(InputStream inputStream, OutputStream outputStream) throws IOException {
-        byte[] bytes = inputStream.readAllBytes();
-        inputStream.close();
+        byte[] bytes = inputStream.readAllBytes(); // inputStream считывает все байты в массиы
+        inputStream.close(); // закрываю inputStream
 
         int length = 0;
 
-        for (byte b : bytes) {
+        for (byte b : bytes) { // через цикл узнаю количесвто четных чисел в bytes
             if ((b & 1) == 0) {
                 length++;
             }
         }
 
-        byte[] sort = new byte[length];
+        byte[] sort = new byte[length]; // создаю массив sort
         int index = 0;
 
-        for (byte b : bytes) {
+        for (byte b : bytes) { // сортирую четные числа из bytes в sort
             if ((b & 1) == 0) {
                 sort[index] = b;
                 index++;
             }
         }
 
-        outputStream.write(sort);
-        System.out.println(Arrays.toString(sort));
-        outputStream.close();
+        outputStream.write(sort); // записываю sort в outputStream
+        System.out.println(Arrays.toString(sort)); // вывожу каждый элемент sort
+        outputStream.close(); // закрываю outputStream
     }
 }
