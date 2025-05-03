e4cfee76e68374f79f53ef78ed6a5b7c6128c1f6
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-05-03 19:47:16 +0300

    задача 4.2.6 третья проверка

--- src/Print.java
+++ src/Print.java
@@ -1,35 +1,19 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.reflect.Array;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class Print {
     public static String print(InputStream inputStream, OutputStream outputStream) throws IOException {
-        byte[] bytes = inputStream.readAllBytes(); // inputStream считывает все байты в массиы
+        byte[] evenNumbers = inputStream.readAllBytes();
 
-        int length = 0;
-
-        for (byte b : bytes) { // через цикл узнаю количесвто четных чисел в bytes
+        for (byte b : evenNumbers) {
             if ((b & 1) == 0) {
-                length++;
+                String str = Arrays.toString(String.valueOf(b).split(" "));
+                outputStream.write(str.getBytes());
             }
         }
 
-        byte[] sort = new byte[length]; // создаю массив sort
-        int index = 0;
-
-        for (byte b : bytes) { // сортирую четные числа из bytes в sort
-            if ((b & 1) == 0) {
-                sort[index] = b;
-                index++;
-            }
-        }
-
-        String str = Arrays.toString(sort);
-        outputStream.write(str.getBytes()); // записываю sort в outputStream
-
-        return str;
+        return outputStream.toString();
     }
 }
